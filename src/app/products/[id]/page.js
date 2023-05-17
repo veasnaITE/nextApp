03a9c15235cb9b3a522c2a0153371746ededce91
Sean Veasna <veasnavk711@gmail.com>
2023-05-17 14:18:51 +0700

    allready

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -4,7 +4,7 @@ export async function generateMetadata({params}){
   return{
       title:product.title,
       description:product.description,
-      thumnail:product.images[0],
+      thumnail:product.images,
       metadataBase: new URL('https://istad.co'),
 alternates: {
   canonical: '/',
@@ -14,7 +14,7 @@ alternates: {
   },
 },
 openGraph: {
-  images:product.images[0],
+  images:product.images,
   title:product.title,
   description:product.description
 },
@@ -27,7 +27,7 @@ export default async function Inherit({ params }) {
     <>
       <main className="container mx-auto flex min-h-screen flex-wrap items-center justify-around p-3 border-0 drop-shadow-sm">
       <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={product.images[0]} alt=""/>
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={product.images} alt=""/>
     <div class="flex flex-col justify-between p-4 leading-normal">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{product.title}</h5>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{product.description}</p>
